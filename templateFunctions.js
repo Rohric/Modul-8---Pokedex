5d2c678a4d3482e39ad06da34880fcb35ad147cd
Rohric <emilmarsal@icloud.com>
2025-09-25 21:22:58 +0200

    add styling light

--- templateFunctions.js
+++ templateFunctions.js
@@ -3,7 +3,7 @@ function templatePokemonCards(name, index) {
     <article class="card">
       <h3>${index + 1}. ${name}</h3>
       <div class="types">${getPokemonTypeBadges(index)}</div>
-      <div class="generation">${PokemonGeneration[index]}</div>
+    <div>  <div class="generation">${PokemonGeneration[index]}</div></div>
       <div><img src="${PokemonImage[index]}" alt="${name}" loading="lazy"></div>
     </article>
   `;
