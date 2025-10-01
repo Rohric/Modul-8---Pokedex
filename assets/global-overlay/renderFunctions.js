3442cbc2fe661dfbf628e460bac73c3225bf5acb
Rohric <emilmarsal@icloud.com>
2025-10-01 13:38:32 +0200

    channge GlovbalOverlay to Pokedex and function system

--- assets/global-overlay/renderFunctions.js
+++ assets/global-overlay/renderFunctions.js
@@ -8,16 +8,20 @@ function renderAllPokemonCards() {
   let ref = document.getElementById("cards");
   ref.innerHTML = "";
 
-  for (
-    let index = 0;
-    index < ShownCards && index < PokemonList.length;
-    index++
-  ) {
+  for (let index = 0; index < ShownCards && index < PokemonList.length; index++) {
     renderOnePokemonCard(index);
   }
 }
 
-function renderGlobalOverlay() {
-  let refGlobalOverlay = document.getElementById("globalOverlay");
-  refGlobalOverlay.innerHTML = templateGlobalOverlay();
+function renderPokedex(index) {
+  currentPokedex = index;
+
+  let overlayRef = document.getElementById('globalOverlay');
+  overlayRef.innerHTML = templatePokedex(index);
+
+  overlayRef.classList.remove('d_none');
+  document.body.classList.add('overlay_open');
+
+  applyOverlayBackdropStyle(index);
+  openPokedexTab('about');           
 }
--- assets/global-overlay/templateFunctions.js
+++ assets/global-overlay/templateFunctions.js
@@ -25,25 +25,18 @@ function templatePokemonCards(name, index) {
 
 
 
-
-function templateGlobalOverlay() {
+function templatePokedex(index) {
   return `
     <div class="overlay_backdrop"></div>
 
     <article class="pokedex">
       <header class="pokedex_header">
-
         <button id="pokedexPrev" class="pokedex_nav_btn" onclick="prevPokedex()">◀</button>
-
-        <div id="pokedexId" class="pokedex_id"></div>
-        <h3 id="pokedexName" class="pokedex_name"></h3>
-        <div id="pokedexGeneration" class="pokedex_generation"></div>
-        <button onclick="closePokedex()" class="pokedex_close">close</button>
-
-      
+        <div id="pokedexId" class="pokedex_id">#${getPokemonId(index)}</div>
+        <h3 id="pokedexName" class="pokedex_name">${PokemonName[index]}</h3>
+        <div id="pokedexGeneration" class="pokedex_generation">${PokemonGeneration[index]}</div>
+        <button onclick="closePokedex()" class="pokedex_close">--X--</button>
         <button id="pokedexNext" class="pokedex_nav_btn" onclick="nextPokedex()">▶</button>
-    
-
       </header>
 
 
@@ -52,47 +45,50 @@ function templateGlobalOverlay() {
       <div id="pokedexTabs" class="pokedex_tabs">
         <button id="tabAbout" class="pokedex_tab is_active" onclick="openPokedexTab('about')">About</button>
         <button id="tabStats" class="pokedex_tab" onclick="openPokedexTab('stats')">Stats</button>
-
-        
       </div>
 
       <!-- ABOUT -->
       <section id="pokedexAbout" class="pokedex_section">
         <section id="pokedexLoreBox" class="pokedex_lore">
-          <div id="pokedexLoreText" class="pokedex_lore_text"></div>
+          <div id="pokedexLoreText" class="pokedex_lore_text">${getPokemonFlavorText(index)}</div>
           <div class="pokedex_lore_meta">
-            <span id="pokedexLoreHeight" class="pokedex_lore_badge"></span>
-            <span id="pokedexLoreWeight" class="pokedex_lore_badge"></span>
+            <span id="pokedexLoreHeight" class="pokedex_lore_badge">Height: ${getPokemonHeight(index)}</span>
+            <span id="pokedexLoreWeight" class="pokedex_lore_badge">Weight: ${getPokemonWeight(index)}</span>
           </div>
         </section>
 
-        <div id="pokedexImage" class="pokedex_image"></div>
-       <div id="pokedexTypes" class="pokedex_types"></div>
+        <div id="pokedexImage" class="pokedex_image">
+          <img src="${PokemonImage[index]}" alt="${PokemonName[index]}">
+        </div>
+
+        <div id="pokedexTypes" class="pokedex_types">
+          ${getPokemonTypeBadges(index)}
+        </div>
       </section>
 
       <!-- STATS -->
       <section id="pokedexStats" class="pokedex_section d_none">
-        <div id="pokedexHP" class="pokedex_stat">
+        <div id="pokedexHP" class="pokedex_stat" style="--value:${getPokemonStat(index, 'hp')}">
           <span class="label">HP</span>
-          <span class="value"></span>
+          <span class="value">${getPokemonStat(index, 'hp')}</span>
           <div class="bar"><div class="fill"></div></div>
         </div>
 
-        <div id="pokedexAttack" class="pokedex_stat">
+        <div id="pokedexAttack" class="pokedex_stat" style="--value:${getPokemonStat(index, 'attack')}">
           <span class="label">Attack</span>
-          <span class="value"></span>
+          <span class="value">${getPokemonStat(index, 'attack')}</span>
           <div class="bar"><div class="fill"></div></div>
         </div>
 
-        <div id="pokedexDefense" class="pokedex_stat">
+        <div id="pokedexDefense" class="pokedex_stat" style="--value:${getPokemonStat(index, 'defense')}">
           <span class="label">Defense</span>
-          <span class="value"></span>
+          <span class="value">${getPokemonStat(index, 'defense')}</span>
           <div class="bar"><div class="fill"></div></div>
         </div>
 
-        <div id="pokedexSpeed" class="pokedex_stat">
+        <div id="pokedexSpeed" class="pokedex_stat" style="--value:${getPokemonStat(index, 'speed')}">
           <span class="label">Speed</span>
-          <span class="value"></span>
+          <span class="value">${getPokemonStat(index, 'speed')}</span>
           <div class="bar"><div class="fill"></div></div>
         </div>
       </section>
